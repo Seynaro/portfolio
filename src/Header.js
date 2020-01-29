d9acda777d4ac840d2c7ed708cd8997231346c81
Khamzat <xamzatseynaro@gmail.com>
2020-01-29 22:41:04 +0300

    create basic markup and components

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <div className="portfolio">
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
