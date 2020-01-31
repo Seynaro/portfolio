1e90d5d4dabc6393d66e641d92f113d1d55b9752
Khamzat <xamzatseynaro@gmail.com>
2020-01-31 22:27:13 +0300

    minor fix

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,13 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <div className={styles.header}>
-      <div className="portfolio">
-
+      <div className={styles.container}>
+         <div className={styles.nav}>
+             <a href="" className={styles.link}>Главная</a>
+             <a href="" className={styles.link}>Скиллы</a>
+             <a href="" className={styles.link}>Проекты</a>
+             <a href="" className={styles.link}>Контакты</a>
+         </div>
       </div>
     </div>
   );
