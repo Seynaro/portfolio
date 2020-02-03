a71b3228c6c28a125912dcc8dee5dc8b89863751
Khamzat <xamzatseynaro@gmail.com>
2020-02-03 20:04:07 +0300

    change main, add photo, greeting

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,20 @@ import React from 'react';
 import styles from './Main.module.css';
 
 function Main() {
-  return (
-    <div className={styles.main}>
-
-    </div>
-  );
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.greeting}>
+                    <span className={styles.greeting}>Привет!</span>
+                    <span className={styles.greeting}>Меня зовут Сейнароев Хамзат.</span>
+                    <span className={styles.greeting}>Я front-end разработчик.</span>
+                </div>
+                <div className={styles.photo}>
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
